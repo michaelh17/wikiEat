1ed8b52fe7943b6e59d114e3b495b2addd669c5d
unknown <michaelxu17@gmail.com>
2022-10-16 02:38:17 +0700

    add popup feature

--- app/src/main/java/com/mobile/example/wikieat/foodCatalog.java
+++ app/src/main/java/com/mobile/example/wikieat/foodCatalog.java
@@ -1,14 +1,75 @@
 package com.mobile.example.wikieat;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class foodCatalog extends AppCompatActivity {
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private TextView namaMakanan;
+    private TextView deskripsi;
+    private Button kembaliButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_catalog);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new foodImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String Name;
+                String Desc;
+
+                if (position == 0){
+                Name = "Onigiri";
+                Desc = "Onigiri adalah nasi kepal versi Jepang yang biasanya berbentuk bola atau segitiga.\n" +
+                        "Di Dalam Onigiri biasanya diisi salmon, ayam, umeboshi, dan tuna (dried plum) lalu\n" +
+                        "kemudian dibungkus dengan nori. Jika Anda baru pertama kali mencobai onigiri , \n" +
+                        "pilihlah tuna dengan mayonese, atau ayam mayonese karena rasanya sangat familiar dengan lidah Indonesia. \n" +
+                        "Onigiri sangat mudah ditemukan di Jepang, hampir semua convenience store menjualnya. ";
+
+                    popupdialog(Name,Desc);
+                }
+                Toast.makeText(foodCatalog.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
+    public void popupdialog(String a,String b){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View popupview = getLayoutInflater().inflate(R.layout.foodpopup,null);
+        deskripsi = (TextView) popupview.findViewById(R.id.deskripsi);
+        namaMakanan = (TextView) popupview.findViewById(R.id.namaMakanan);
+        kembaliButton = (Button) popupview.findViewById(R.id.kembaliButton);
+
+        namaMakanan.setText(a);
+        deskripsi.setText(b);
+
+        dialogBuilder.setView(popupview);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        kembaliButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/mobile/example/wikieat/homepageActivity.java
+++ app/src/main/java/com/mobile/example/wikieat/homepageActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 public class homepageActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
-    Button infoButton;
+    Button infoButton, foodButton , drinkButton, uploadButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,9 @@ public class homepageActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         Intent intent = getIntent();
         infoButton = findViewById(R.id.infoButton);
+        foodButton = findViewById(R.id.foodButton);
+        drinkButton = findViewById(R.id.drinkButton);
+
         TextView userLogin = findViewById(R.id.userLogin);
         TextView logout = findViewById(R.id.logoutText);
         Bundle getter = intent.getExtras();
@@ -37,6 +42,12 @@ public class homepageActivity extends AppCompatActivity {
             logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(logoutIntent);
         });
+
+        foodButton.setOnClickListener(view->{
+            Intent food = new Intent(this,foodCatalog.class);
+            startActivity(food);
+        });
+
 
 
     }
