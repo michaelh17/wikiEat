70323b5cc46e14015a32888c7da59ae7b31c2455
unknown <michaelxu17@gmail.com>
2022-10-16 14:45:41 +0700

    adding beverage description

--- app/src/main/java/com/mobile/example/wikieat/foodCatalog.java
+++ app/src/main/java/com/mobile/example/wikieat/foodCatalog.java
@@ -97,6 +97,65 @@ public class foodCatalog extends AppCompatActivity {
                     Harga = "Rp. 10.000 - 20.000";
                     popupdialog(Name,Desc,Harga);
                 }
+
+                else if (position == 6){
+                    Name = "Matcha";
+                    Desc = "Matcha menjadi minuman khas Jepang yang sudah terkenal di berbagai negara.\n\n" +
+                            "Minuman ini terbuat dari bubuk teh hijau yang cukup dituang air dan diaduk hingga berbusa. " +
+                            "Matcha kerap disajikan dalam bentuk minuman. \n\n" +
+                            "Namun  Matcha dapat dikonsumsi sebagai topping kue, cookies, permen, latte, dan lain sebagainya.";
+                    Harga = "Rp. 10.000 - 25.000";
+                    popupdialog(Name,Desc,Harga);
+                }
+
+                else if (position == 7){
+                    Name = "Teh Sakura";
+                    Desc = "Teh sakura ini menjadi minuman khas Jepang yang cukup populer.\n\n" +
+                            "Pasalnya, minuman ini terbuat dari bunga musim semi di Jepang, Bunga Sakura. " +
+                            "Bunga sakura yang diseduh dan dijadikan teh akan terasa nikmat. " +
+                            "Teh ini cocok diminum sambil menikmati musim semi di Jepang.";
+                    Harga = "Rp. 10.000 - 25.000";
+                    popupdialog(Name,Desc,Harga);
+                }
+
+                else if (position == 8){
+                    Name = "Sake";
+                    Desc = "Sake merupakan minuman khas Jepang yang terkenal di dunia.\n\n" +
+                            "Minuman beralkohol ini dibuat dari fermentasi beras. " +
+                            "Rasa Sake Jepang bergantung pada aspek jenis beras, waktu fermentasi, dan lain sebagainya. " +
+                            "Rasanya bercampur antara rasa alkohol penuh umami dengan rasa manis seperti wine. " +
+                            "Sake di setiap wilayah di Jepang memiliki rasa yang unik khas lokal.";
+                    Harga = "Rp. 80.000 - 900.000";
+                    popupdialog(Name,Desc,Harga);
+                }
+
+                else if (position == 9){
+                    Name = "Uroncha / Teh Oolong";
+                    Desc =  "Nama lain teh Uroncha adalah teh olong.\n\n" +
+                            "Perbedaan Uroncha daripada teh lainnya adalah dalam proses pembuatannya. " +
+                            "Teh hitam, teh hijau dan Uroncha memang berasal dari daun teh yang sama. Namun, proses oksidasi Uroncha memberikan aroma yang kuat. " +
+                            "Minuman ini biasanya diminum saat makan siang untuk menenangkan perut.";
+                    Harga = "Rp. 10.000 - 50.000";
+                    popupdialog(Name,Desc,Harga);
+                }
+
+                else if (position == 10){
+                    Name = "Shochu";
+                    Desc =  "Shochu rasanya seperti vodka dan terbuat dari beras, ubi jalar, tebu, dan barley. " +
+                            "Rasa Sochu tergantung dari cara penyeduhannya. Varian dari ubi jalar merupakan sochu yang paling terkenal di Jepang.";
+                    Harga = "Rp. 50.000 - 600.000";
+                    popupdialog(Name,Desc,Harga);
+                }
+
+                else if (position == 11){
+                    Name =  "Ramune (ラムネ)";
+                    Desc =  "Ramune adalah minuman berkarbonasi non alkohol yang namanya berasal dari bahasa Inggris lemonade.\n\n" +
+                            "Dalam bahasa Jepang, lemonade diucapkan remonedo (レモネード), " +
+                            "yang mungkin salah didengar saat pertama kali diperkenalkan di Jepang atau mengalami perubahan bahasa seiring waktu sehingga menjadi ramune. " +
+                            "Minuman Ramune sangatlah terkenal di Jepang bahkan di Indonesia sekalipun karena rasanya yang sangat segar.";
+                    Harga = "Rp. 20.000 - 100.000";
+                    popupdialog(Name,Desc,Harga);
+                }
             }
         });
 
